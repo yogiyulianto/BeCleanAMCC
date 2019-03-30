04a2a45bc07375842c12926ec2a38f60b52b9f2b
yogiyulianto <yogi@bringin.id>
2019-03-30 11:49:46 +0700

    buat yogi activity

---
+++ app/src/main/java/or/id/amcc/beclean/YogiActivity.java
@@ -0,0 +1,13 @@
+package or.id.amcc.beclean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YogiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yogi);
+    }
+}
