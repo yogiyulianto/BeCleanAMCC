bc9187bb910a642b6171bc132eff284f83f8d23a
Ajiewahyu24 <Ajiewahyu24@gmail.com>
2019-04-07 12:18:11 +0700

    gambar dan button

--- app/src/main/java/or/id/amcc/beclean/BerandaActivity.java
+++ app/src/main/java/or/id/amcc/beclean/BerandaActivity.java
@@ -8,6 +8,6 @@ public class BerandaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aji);
+        setContentView(R.layout.activity_beranda);
     }
 }
