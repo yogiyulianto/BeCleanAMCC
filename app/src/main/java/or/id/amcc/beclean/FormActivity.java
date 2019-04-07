bed576ed861cd75a90dc9b5372312c6f34419c1b
Baskoro99 <maxitune99@gmail.com>
2019-04-07 13:48:30 +0700

    add form

--- app/src/main/java/or/id/amcc/beclean/FormActivity.java
+++ app/src/main/java/or/id/amcc/beclean/FormActivity.java
@@ -3,7 +3,7 @@ package or.id.amcc.beclean;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FormActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
